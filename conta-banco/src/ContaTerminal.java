11824160d48190dcad8ba1ddeae099bb20369b3f
jotave <joaovitorsouzaa31@gmail.com>
2025-08-31 17:59:28 -0300

    projeto finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -15,14 +15,11 @@ public class ContaTerminal {
         String name = scanner.nextLine();
 
         System.out.println("Digite o seu saldo:");
-String balanceInput = scanner.next().replace(",", ".");
-double balance = Double.parseDouble(balanceInput);
+        String balanceInput = scanner.next().replace(",", ".");
+        double balance = Double.parseDouble(balanceInput);
 
 
-        System.out.printf(
-            "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n",
-            name, numberAgency, numberBankAccount, balance
-        );
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n", name, numberAgency, numberBankAccount, balance);
 
         scanner.close();
     }
